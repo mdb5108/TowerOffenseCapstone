f8c2d534f44ae2df8a5e257251d0bc9a52d78073
Michael Brown <gamer5108@hotmail.com>
2015-11-17 21:51:48 -0700

    Initial commit from command line

---
+++ Source/TowerOffense/TowerOffenseGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TowerOffense.h"
+#include "TowerOffenseGameMode.h"
+
+
+
+
---
+++ Source/TowerOffense/TowerOffenseGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TowerOffenseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWEROFFENSE_API ATowerOffenseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
