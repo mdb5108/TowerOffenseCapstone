10f7a72151ae023cc020a354da6d8887f30abded
shreyasdarshan <shreyas.darshan@gmail.com>
2015-11-24 01:24:37 -0700

    added code for minnion taking damage

--- Source/TowerOffense/Private/VikingMinion.cpp
+++ Source/TowerOffense/Private/VikingMinion.cpp
@@ -3,6 +3,7 @@
 #include "TowerOffense.h"
 #include "VikingMinion.h"
 #include "PathNode.h"
+//#include "BasicClasses.h"
 
 // Sets default values
 AVikingMinion::AVikingMinion()
@@ -11,24 +12,52 @@ AVikingMinion::AVikingMinion()
 	PrimaryActorTick.bCanEverTick = true;
 
     movementComp = static_cast<UCharacterMovementComponent*>(this->GetComponentByClass(UCharacterMovementComponent::StaticClass()));
+
+	//increment minion count
+	AVikingMinion::count_of_minion++;
+	
+	
+	
 }
 
 // Called when the game starts or when spawned
 void AVikingMinion::BeginPlay()
 {
 	Super::BeginPlay();
-
+	UCapsuleComponent* capComp = nullptr;
     curState = AI_STATE::FOLLOW_PATH;
+
+	TArray<UCapsuleComponent*> Components;
+	//this->GetComponents<UStaticMeshComponent>(Stat_Components);
+	this->GetComponents<UCapsuleComponent>(Components);
+	for (int32 i = 0; i<Components.Num(); i++)
+	{
+		//UStaticMeshComponent* StaticMeshComponent = Components[i];
+		capComp = Components[i];
+		//UE_LOG(LogTemp, Warning, TEXT("My Object's location is "));
+		//UStaticMesh* StaticMesh = StaticMeshComponent->StaticMesh;
+	}
+	
+
+	//OnActorHit.AddDynamic(this, &AVikingMinion::on_collision);
+	//StaticMeshComponent->OnComponentHit.AddDynamic(this, &AVikingMinion::on_collsion);
+	capComp->OnComponentHit.AddDynamic(this, &AVikingMinion::on_collision);
+	
 }
+
+//void AVikingMinion::on_collision(class AActor* otherActor, class UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 
 // Called every frame
 void AVikingMinion::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
+	//UE_LOG(LogTemp, Warning, TEXT("Delta Time %f"),DeltaTime)
+	delta_local += DeltaTime;
     switch(curState)
     {
-        case FOLLOW_PATH:
+		case AI_STATE::FOLLOW_PATH:
             if(currentNode != NULL)
             {
                 UCharacterMovementComponent::FStepDownResult* outStepDownResult = NULL;
@@ -43,18 +72,38 @@ void AVikingMinion::Tick( float DeltaTime )
                 SetActorRotation(direction.Rotation());
             }
             break;
-        case HOSTILE:
+        case AI_STATE::HOSTILE:
             break;
+		case AI_STATE::DEAD:
+			//speed = 2.0f;
+			break;
         default:
             checkf(false, TEXT("Ran into unknown state!"));
             break;
+
+			
+
     }
 }
 
 // Called to bind functionality to input
 void AVikingMinion::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 {
-	Super::SetupPlayerInputComponent(InputComponent);
-
+	Super::SetupPlayerInputComponent(InputComponent);	
 }
 
+void AVikingMinion::on_collision(class AActor* otherActor, class UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (otherActor->ActorHasTag("FlamingBall") && delta_local >= 2)
+	{
+		current_health--;
+		UE_LOG(LogTemp, Warning, TEXT("Current health %f"),current_health);
+		if (current_health <= 0)
+		{
+			curState = AI_STATE::DEAD;					
+		}
+		delta_local = 0;
+		UE_LOG(LogTemp, Warning, TEXT("Collider triggered"));
+	}
+	//UE_LOG(LogTemp, Warning, TEXT("My Object's location is %s"), *ActorItr->GetActorLocation().ToString());
+}
